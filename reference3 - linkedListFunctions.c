ed0dc6b1672f160c8c68365638e631c4afc18832
Ryan Keh <113022074+ryankeh@users.noreply.github.com>
2023-01-26 02:24:36 +0800

    Update reference3 - linkedListFunctions.c

--- reference3 - linkedListFunctions.c
+++ reference3 - linkedListFunctions.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+void printList (ListNode *head)
+ListNode * findNode(ListNode *head, int index)
+
+
+int main() {
+    printf("Hello world");
+    return 0;
+}
+
+void printList(ListNode *head){
+    if (head == NULL)
+        return;
+    while (head != NULL){
+        printf("%d ", head->item);
+        head = head->next;
+    }
+    printf("\n");
+}
+
+ListNode * findNode(ListNode *head, int index){
+    if (head == NULL || index < 0)
+        return NULL;
+    while (index > 0){
+        head = head->next;
+        if (head == NULL)
+            return NULL;
+        index--;
+    }
+    return head;
+}
