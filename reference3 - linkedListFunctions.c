ac76659028c303ef93c138c145a0d58ca7f42c7a
Ryan Keh <113022074+ryankeh@users.noreply.github.com>
2023-01-29 03:38:49 +0800

    Update reference3 - linkedListFunctions.c

--- reference3 - linkedListFunctions.c
+++ reference3 - linkedListFunctions.c
@@ -7,9 +7,28 @@ int insertNode(ListNode **ptrHead, int index, int value)
 int removeNode(ListNode **ptrHead, int index)
 int sizeList(ListNode *head)
 
-
+//main function uses listsize to keeptrack instead of sizeList function to reduce processing power required
 int main() {
-    printf("Hello world");
+    ListNode *head = NULL;
+    int listsize = 0;
+    srand(time(NULL));
+    while (listsize < 10){
+        insertNode(&head, 0, rand() % 100);
+        listsize++;
+        printf("List: ");
+        printList(head);
+        printf("\n");
+    }
+    printf("%d nodes\n", listsize);
+    
+    while (size > 0){
+        removeNode(&head, listsize-1);
+        listsize--;
+        printf("List: ");
+        printList(head);
+        printf("\n");
+    }
+    printf("%d nodes\n", listsize);
     return 0;
 }
 
