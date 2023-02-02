7be11431cce5586d6e176aa22c572fd63dc5533a
Ryan Keh <113022074+ryankeh@users.noreply.github.com>
2023-02-02 17:49:28 +0800

    Update Lab 1.3.c

--- Lab 1.3.c
+++ Lab 1.3.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -10,40 +11,44 @@ typedef struct _listnode{
 void printList(ListNode *cur);
 ListNode * findNode(ListNode *cur, int index);
 int insertNode(ListNode **ptrHead, int index, int item);
+void deleteList(ListNode **ptrHead);
 
-int removeNode(ListNode **ptrHead,int index);
-void split(ListNode *head, ListNode **ptrEvenList, ListNode **ptrOddList); 
+int split(ListNode *cur,ListNode **ptrEvenList,ListNode **ptrOddList);
 
 int main()
 {
     ListNode *head=NULL;
+    ListNode *oddHead = NULL;
+    ListNode *evenHead = NULL;
+
     int size =0;
     int item;
-    int index;
 
     printf("Enter a list of numbers, terminated by any non-digit character: \n");
     while(scanf("%d",&item))
         if(insertNode(&head,size, item)) size++;
     scanf("%*s");
 
+    printf("\nBefore split() is called:\n");
+    printf("The original list:\n");
     printList(head);
 
-    while(1){
-        printf("Enter the index of the node to be removed: ");
-        scanf("%d",&index);
+    split(head, &evenHead, &oddHead);
 
-        if(removeNode(&head,index))
-            size--;
-        else{
-            printf("The node cannot be removed.\n");
-            break;
-        }
-
-        printf("After the removal operation,\n");
-        printList(head);
-    }
-
+    printf("\nAfter split() was called:\n");
+    printf("The original list:\n");
     printList(head);
+	printf("The even list:\n");
+	printList(evenHead);
+	printf("The odd list:\n");
+	printList(oddHead);
+
+	if(head!=NULL)
+       deleteList(&head);
+    if(oddHead!=NULL)
+       deleteList(&oddHead);
+    if(evenHead!=NULL)
+       deleteList(&evenHead);
     return 0;
 }
 
@@ -91,31 +96,21 @@ int insertNode(ListNode **ptrHead, int index, int item){
     return 0;
 }
 
-int removeNode(ListNode **ptrHead,int index)
-{
-    ListNode *pre, *cur;
-    
-    if (index == 0){
-        cur = (*ptrHead)->next;
-		free((*ptrHead));
-		(*ptrHead) = cur;
-        return 1;
-    }
-        
-    else if ((pre = findNode(*ptrHead, index-1)) != NULL){
-        if (pre->next == NULL){
-            return 0;
-        }
-		cur = pre->next;
-		pre->next = cur->next;
+void deleteList(ListNode **ptrHead){
+    ListNode *cur = *ptrHead;
+    ListNode *temp;
+    while (cur!= NULL) {
+		temp=cur->next;
 		free(cur);
-		return 1;
-    }
-    return 0;
+		cur=temp;
+	}
+	*ptrHead=NULL;
 }
 
-split(ListNode *head, ListNode **ptrEvenList, ListNode **ptrOddList){
+int split(ListNode *cur, ListNode **ptrEvenList,ListNode **ptrOddList)
+{
     int i=0,oddIndex=0,evenIndex=0;
+    
     while(1){
         if(findNode(*ptrHead, i)) == NULL){
             printList(*ptrEvenList);
