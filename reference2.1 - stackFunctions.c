a860c3d0f6957bce3e7a4c9315ace119b429acad
Ryan Keh <113022074+ryankeh@users.noreply.github.com>
2023-02-08 12:23:06 +0800

    Update reference2.1 - stackFunctions.c

--- reference2.1 - stackFunctions.c
+++ reference2.1 - stackFunctions.c
@@ -23,3 +23,23 @@ int isEmptyStack(Stack *s){
     if ((s->ll).size == 0) return 1;
     return 0;
 }            
+
+            
+//Application reverseString
+int main(){
+    int i = 0;
+    Stack s;
+    s.ll.head = NULL;
+    
+    printf("Enter a number: ");
+    scanf("%d", &i);
+    while (i != -1){
+        push(&s, i);
+        printf("Enter a number: ");
+        scanf("%d", &i);
+    }
+    printf("Popping stack: ");
+    while (!isEmptyStack(&s))
+        printf("%d ", pop(&s));
+    return 0;
+}
