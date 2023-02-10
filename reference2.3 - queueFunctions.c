cd83598ab434b87a67548652630b1a9bd673867c
Ryan Keh <113022074+ryankeh@users.noreply.github.com>
2023-02-10 17:48:03 +0800

    Update reference2.3 - queueFunctions.c

--- reference2.3 - queueFunctions.c
+++ reference2.3 - queueFunctions.c
@@ -5,3 +5,10 @@ typedef struct _queue{
 void enqueue(Queue *q, int item){
     insertNode(&(q->ll), q->ll.size, item);
 }
+
+int dequeue(Queue *q){
+        int item;
+        item = ((q->ll).head)->item;
+        removeNode(&ll, 0);
+        return item;
+}
