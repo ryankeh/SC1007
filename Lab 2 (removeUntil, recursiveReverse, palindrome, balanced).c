65c7e9f4aa19589560795c54367e167f559c69f8
Ryan Keh <113022074+ryankeh@users.noreply.github.com>
2023-02-12 03:14:56 +0800

    Update Lab 2 (removeUntil, recursiveReverse, palindrome, balanced).c

--- Lab 2 (removeUntil, recursiveReverse, palindrome, balanced).c
+++ Lab 2 (removeUntil, recursiveReverse, palindrome, balanced).c
@@ -144,8 +144,41 @@ void recursiveReverse(Queue *q){
 //Question 3
 
 int palindrome(char *word){
-
-	// write your code here
+int i=0, length;
+    printf("Enter a string: %s\n", &word[i]);
+    // for(int i = 0; word[i]; i++){
+    //     word[i] = tolower(word[i]);
+    // }
+    
+    Stack s;
+    //initialize temp stack
+    s.ll.head = NULL;
+	s.ll.size = 0;
+	s.ll.tail = NULL;
+	
+	Queue q;
+    //initialize temp queue
+    q.ll.head = NULL;
+	q.ll.size = 0;
+	q.ll.tail = NULL;
+    
+    length = strlen(word);
+    for(i=0;i<length;i++){
+        if(word[i]!=32){
+            push(&s,word[i]);
+            enqueue(&q,word[i]);
+        }
+    }
+    
+    while(!isEmptyStack(&s)){
+        printf("%d - %d\n", dequeue(&q), pop(&s));
+        // if(dequeue(&q)!=pop(&s)){
+        //     printf("The string is not a palindrome.\n");
+        //     return -1;
+        // }
+    }
+    printf("The string is a palindrome.\n");
+    return 0;
 }
 
 
