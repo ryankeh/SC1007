0998553a640b3d426c9216eb25ab89aed205e681
Ryan Keh <113022074+ryankeh@users.noreply.github.com>
2023-02-16 16:56:34 +0800

    Update reference3.1 - binaryTree.c

--- reference3.1 - binaryTree.c
+++ reference3.1 - binaryTree.c
@@ -3,3 +3,10 @@ typedef struct _btnode{
     struct _btnode *left;
     struct _btnode *right;
 } BTNode;
+
+Void TreeTraversal2(BTNode *cur){
+    If (cur == NULL) return;
+    PrintNode(cur); // visit cur
+    TreeTraversal2(cur->left);
+    TreeTraversal2(cur->right);
+}
