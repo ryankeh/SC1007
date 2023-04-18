d9c2266d63cbfc878994c940fbf44752899e6835
Ryan Keh <113022074+ryankeh@users.noreply.github.com>
2023-04-18 22:10:18 +0800

    Create Lab 5.2 (dualSortedSearch).c

---
+++ Lab 5.2 (dualSortedSearch).c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void quickSort(int A[], int low, int high);
+int partition (int A[], int low, int high);
+
+void dualSortedSearch (int[],int, int, int[]);
+
+int main()
+{
+    int i,size,K;
+    int* A;
+    int index[2] = {-1,-1};
+
+    //Search key
+    scanf("%d",&K);
+
+    //create an array
+    scanf("%d",&size);
+    A=(int *)malloc(sizeof(int)*size);
+    for(i=0; i<size; i++)
+    {
+        scanf("%d",&A[i]);
+    }
+
+    quickSort(A,0,size-1);
+    dualSortedSearch(A,size,K,index);
+
+    if(index[0]!=-1)
+        printf("%d %d\n",index[0], index[1]);
+    else
+        printf("Not found");
+
+    free(A);
+    return 0;
+}
+
+void dualSortedSearch(int A[], int size, int K, int dualIndex[])
+{
+   int i=0,j=0;
+   for(i=0;i<size;i++){
+       for(j=0;j<size;j++){
+           if(A[i]+A[j]==k){
+               dualIndex[0]=A[i];
+               dualIndex[1]=A[j];
+           }
+           if(A[i]+A[j]>k){
+               break;
+           }
+       }
+   }
+}
+
+int partition (int A[], int low, int high)
+{
+    int pivot = A[high];
+    int i = (low - 1);
+
+    int temp;
+    for (int j = low; j <= high- 1; j++)
+    {
+        if (A[j] < pivot)
+        {
+            i++;
+
+            temp = A[i];
+            A[i] = A[j];
+            A[j] = temp;
+        }
+    }
+
+    temp = A[i+1];
+    A[i+1]= A[high];
+    A[high] = temp;
+    return (i + 1);
+}
+
+void quickSort(int A[], int low, int high)
+{
+    if (low < high)
+    {
+        int pivot = partition(A, low, high);
+
+        quickSort(A, low, pivot - 1);
+        quickSort(A, pivot + 1, high);
+    }
+}
